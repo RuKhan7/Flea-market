290f01d99634b9750bc44ef450c57320818c9885
Студент группы IS <is@sielom.local>
2025-10-15 10:28:48 +0500

    Доделал

--- Flea_market/Flea_market/settings.py
+++ Flea_market/Flea_market/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Flea_market_app'
 ]
 
 MIDDLEWARE = [
--- Flea_market/Flea_market_app/admin.py
+++ Flea_market/Flea_market_app/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(User)
-admin.site.register()
-admin.site.register()
-admin.site.register()
-admin.site.register()
-admin.site.register()
-admin.site.register()
-
+admin.site.register(Profile)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(ProductCategory)
+admin.site.register(Comment)
+admin.site.register(Review)
+admin.site.register(ReviewComment)
 
 
 
